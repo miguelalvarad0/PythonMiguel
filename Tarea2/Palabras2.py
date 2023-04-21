22c291584b3a4026a080156d51af8158330a728b
Miguel Alvarado <mikel@MacBook-Pro-de-Miguel.local>
2023-04-21 17:48:17 -0600

    agregué comentarios al codigo

--- Tarea2/Palabras2.py
+++ Tarea2/Palabras2.py
@@ -1,3 +1,6 @@
+#defino la funcion donde se valida que ambas cadenas de caracteres ingresados contegan la misma cantidad caracteres
+#y de ser asi, se procede a imprimir en pantalla de manera intercalada/combinada, los caracteres de las cadenas ingresadas por el usuario
+#se utiliza el largo de la cadena de caracteres como numero de veces que se corre el loop en la funcion 
 def combinar_palabras(string11, string22):
 
     if len(string11) != len(string22):
@@ -10,6 +13,8 @@ def combinar_palabras(string11, string22):
             print(string22[i],end="")
         print("\n")
 
+#se define la funcion en la cual se verifica que los caracteres de las palabras no sean numeros sino letras
+#la funcion retorna un '1' o un '0' si la verificacione o no exitosa 
 def validar_palabra(palabra,largo):
 
     contador_strings = 0
@@ -26,7 +31,7 @@ def validar_palabra(palabra,largo):
         return 1
 
 
-
+#se piden al usuario las palabras a ser combninadas, se almacena el largo de estas en variables y se usan como parametro de entradas para llamar la funcion validar_palabra
 string1 = input("Inserte una palabra:")
 string2 = input("Inserte otra palabra con la misma cantidad de letras: ")
 largo_palabra1 = len(string1)
@@ -34,6 +39,7 @@ largo_palabra2 = len(string2)
 validar_palabra(string1,largo_palabra1)
 validar_palabra(string2,largo_palabra2)
 
+#si ambas palabras contienen unicamente letras, se llama la funcio combinar_palabras; caso contrario se finaliza el programa
 if ((validar_palabra(string1,largo_palabra1) == 0) and (validar_palabra(string2,largo_palabra2) == 0)):
     combinar_palabras(string1,string2)
     print("FIN DEL PROGRAMA\n")    
