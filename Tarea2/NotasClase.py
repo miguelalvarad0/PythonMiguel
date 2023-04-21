b613d14f11b500f3804da066f6ba13c595259c0e
Miguel Alvarado <mikel@MacBook-Pro-de-Miguel.local>
2023-04-21 10:12:53 -0600

    modificacion en codigo para que soporte cambios en los nombres, notas y cantidad de cursos

--- Tarea2/NotasClase.py
+++ Tarea2/NotasClase.py
@@ -1,3 +1,15 @@
+def contar_cursos(sample_dictionary):
+    largo = len(sample_dictionary["class"]["student"]["marks"])
+    return largo 
+
+def calcular_promedio(sample_dictionary,cantidad_cursos):
+    suma_notas = 0
+    g = list(sample_dictionary["class"]["student"]["marks"].values())
+
+    for i in range(0,cantidad_cursos,1):
+        suma_notas = suma_notas + g[i]
+    promedio = suma_notas / cantidad_cursos
+    return promedio
 
 sampleDict = {
        "class": {
@@ -6,22 +18,23 @@ sampleDict = {
                "marks": {
                    "physics": 70,
                    "history": 80,
-                    "math": 90, }
+                    "math": 90
+                       }
 } }
 }
 
-physics = sampleDict["class"]["student"]["marks"]["physics"] 
-history = sampleDict["class"]["student"]["marks"]["history"]
-math = sampleDict["class"]["student"]["marks"]["math"]
-
-nombre = sampleDict["class"]["student"]["name"]
-
-promedio = (physics+history+math)/3
-
 miDict = {
-    "nombre": nombre,
-    "nota": promedio
+    "nombre": "nombre",
+    "nota": "promedio"
 }
 
-print("Nombre:",miDict["nombre"],", Promedio: ",miDict["nota"])
+a = contar_cursos(sampleDict)
+b = calcular_promedio(sampleDict,a)
+miDict["nombre"] = sampleDict["class"]["student"]["name"]
+miDict["nota"] = b
+
+print("Nombre:",miDict["nombre"],", Promedio: ",end="")
+print(b)
+print("\n")
+
 
