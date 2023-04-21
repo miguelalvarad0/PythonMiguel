4c467b374da8c01157b2ffd2b433f3e65d74d0d2
Miguel Alvarado <mikel@MacBook-Pro-de-Miguel.local>
2023-04-21 16:16:54 -0600

    agregué comentarios al codigo

--- Tarea2/Factorial.py
+++ Tarea2/Factorial.py
@@ -1,4 +1,14 @@
-def hola(x):
+#Definir la funcion que recibe caracteres, determina si es un numero natural, y devuelve el resultado del calculo factorial
+
+
+
+def calcular_factorial(x):
+# se intenta convertir el\los caracteres de entrada a tipo entero y se valida que sea menor a 0,
+# en caso de que no se cumpla entonces no se calcula el factorial y se devuelve un msj al usuario indicando que 
+# el valor insertado no es valido.
+# en caso de que se validen los caracteres como un entero se calcula el factorial mediante un ciclo for
+#la cantidad de loops del ciclo for esta definido por el largo del caracter ingresado por el usuario
+
     try:
         z = int(x)
         
@@ -14,7 +24,10 @@ def hola(x):
         print("No es un numero valido")
         print("Fin del Programa\n")
 
+
+#Imprimir Titulo del programa, solicitar un numero al usuario, e invocar la funcion factorial
 print("\nCalculadora de Factorial\n")
 num = input("Ingrese un numero natural:")
-hola(num)
+calcular_factorial(num)
+         
          
